5cf3c470a1c8a46d4ff695d3b6a27736cbc6aa1d
v@v <v@v>
2021-05-11 04:30:50 -0700

    commit

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
+# run in a while loop
 # while true; do node index.js|tee -a console.log; sleep 10; done
+
+# Or call directly
 node index.js |tee -a console.log &
+
+# Or add the following at cront to run every minute
+# * * * * *  cd /home/ubuntu/auto_repay/ && node index.js >> console.log 2>&1
+
